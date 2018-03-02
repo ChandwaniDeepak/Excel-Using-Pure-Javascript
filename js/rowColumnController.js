dff82805f8bfbe8bae9effbd25e09f0a06e2cea5
Deepak Chandwani <34138972+ChandwaniDeepak@users.noreply.github.com>
2018-03-02 18:19:15 -0500

    Unique id assigned to dynamically generating Rows cells

--- js/rowColumnController.js
+++ js/rowColumnController.js
@@ -1,23 +1,27 @@
 function addRow()
 {
     let myTable = document.getElementById("myTable");
+    let myTableHead = document.querySelectorAll("th");
     let columnCount = myTable.rows[0].cells.length;
-    let columnHead = myTable.rows[0].cells;
+    var columnHead = myTable.rows[0].cells;
 
     let addRow = myTable.insertRow(myTable.rows.length);
     let rowCount = myTable.rows.length - 1;
     for(let i = 0; i < columnCount; i++)
     {
         let cell = addRow.insertCell(i);
-        cell.id = columnHead[i+1]+rowCount;
+        cell.id = (myTableHead[i+1].innerHTML)+rowCount;
         cell.contentEditable = 'true';
-        console.log("column id "+columnHead[i+1]+rowCount);
+        console.log("column id "+(myTableHead[i+1].innerHTML)+rowCount);
+        //console.log("Head id "+(myTableHead[i+1].innerHTML));
+        //table.rows[1].cells[i].headers
         if(i == 0)
         {
             //cell.id = columnHead[i+1]+rowCount;
             cell.contentEditable = 'false';
             cell.innerHTML = rowCount.toString();
-            console.log("column id"+columnHead[i+1]+rowCount);
+            cell.style.textAlign = 'center';
+            //console.log("column id "+(myTableHead[i+1].innerHTML)+rowCount);
          }
     }
     //alert("Row count: "+myTable.rows.length);
