1354cac683bd875d17db8a87f63b8cfe67afb65e
deosthale.s <deosthale.s@husky.neu.edu>
2018-03-03 22:07:26 -0500

    Added remove coloumn functionality

--- js/rowColumnController.js
+++ js/rowColumnController.js
@@ -109,6 +109,25 @@ function addColumn() {
     }
 }
 
+function removeColumn()
+{
+    var tbl = document.getElementById('myTable'), // table reference
+        lastCol = tbl.rows[0].cells.length - 1,    // set the last column index
+        i, j;
+    console.log('Table row: '+tbl.rows.length);
+    console.log('Table lastcol: '+lastCol);
+    if(lastCol >= 1){
+        // delete cells with index greater then 0 (for each row)
+        for (i = 0; i < tbl.rows.length; i++) {
+            //for (j = lastCol; j > 0; j--) {
+                tbl.rows[i].deleteCell(lastCol);
+            //}
+        }
+    }else {
+        alert('cannot delete first column');
+    }
+}
+
 
 
 
